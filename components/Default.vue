ea666e2555bf9f4926237501892f32b19d50974d
Kelvin <kelvin@amus-MacBook-Pro.local>
2023-11-24 07:28:41 -0800

    create Default components

---
+++ components/Default.vue
@@ -0,0 +1,33 @@
+<template>
+     <div>
+          <div class="bg-red-400 p-4">
+               <Menubar :model="items">
+                    <template #start>
+                         <h1>MEMORIES HUB</h1>
+                    </template>
+                    <template #end>
+                         <a href="/profile"> <i class="pi pi-fw pi-user"></i>Account</a>
+                    </template>
+               </Menubar>
+          </div>
+          <div class="content">
+                <slot></slot>
+          </div>
+     </div>
+   
+ </template>
+ 
+ <script setup>
+ import Menubar from 'primevue/menubar';
+ const   items = [
+                 {
+                    label:'All Posts',
+                    icon:'pi pi-fw pi-file',
+                 }]
+ </script>
+
+ <style>
+ .content{
+     margin:2%  5%;
+ }
+</style>
